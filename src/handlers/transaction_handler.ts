afc718669e7353876f3f5cf506f8a4d0f9e03dd1
stefaniatadama <stefania.damato.15@um.edu.mt>
2021-08-11 16:31:29 +0100

    Fixed formatting

--- src/handlers/transaction_handler.ts
+++ src/handlers/transaction_handler.ts
@@ -1,40 +1,46 @@
-import {TransactionDB } from '../db/models/transaction';
+import {TransactionDB} from '../db/models/transaction';
 import {io} from '../server';
 
 export class TransactionHandler {
-    listTransactionsByAddress = (addr: string) => {
-        return new Promise((resolve: Function, reject: Function) => {
-          return TransactionDB.aggregate([
-              {$match: {"tx.body.messages.@type": "/cosmos.bank.v1beta1.MsgSend"}},
-              {$match: {$or: [{"tx.body.messages.from_address": addr}, {"tx.body.messages.to_address": addr}]}},
-            ],
-            (err, res) => {
-            if (err) {
-              reject(err);
-            } else {
-              io.emit('list sends and receives for a given address', res);
-              resolve(res);
-            }
-          });
+  listTransactionsByAddress = (addr: string) => {
+    return new Promise((resolve: Function, reject: Function) => {
+      return TransactionDB.aggregate([
+        {$match: {"tx.body.messages.@type": "/cosmos.bank.v1beta1.MsgSend"}},
+        {$match: {
+            $or: [
+              {"tx.body.messages.from_address": addr}, {"tx.body.messages.to_address": addr}
+            ]
+          }
+        },
+      ],
+        (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            io.emit('list sends and receives for a given address', res);
+            resolve(res);
+          }
         });
-    };
+    });
+  };
 
-    listTransactionsByAddressByAsset = (addr: string, asset: string) => {
-      return new Promise((resolve: Function, reject: Function) => {
-        return TransactionDB.aggregate([
-            {$match: {"tx.body.messages.@type": "/cosmos.bank.v1beta1.MsgSend"}},
-            {$match: {
-              "tx.body.messages": {
-                $elemMatch: {
-                  $or: [
-                    {"from_address": addr}, {"to_address": addr}
-                  ],
-                  "amount.denom": asset
-                }
+  listTransactionsByAddressByAsset = (addr: string, asset: string) => {
+    return new Promise((resolve: Function, reject: Function) => {
+      return TransactionDB.aggregate([
+        {$match: {"tx.body.messages.@type": "/cosmos.bank.v1beta1.MsgSend"}},
+        {$match: {
+            "tx.body.messages": {
+              $elemMatch: {
+                $or: [
+                  {"from_address": addr}, {"to_address": addr}
+                ],
+                "amount.denom": asset
               }
-            }},
-          ],
-          (err, res) => {
+            }
+          }
+        },
+      ],
+        (err, res) => {
           if (err) {
             reject(err);
           } else {
@@ -42,23 +48,28 @@ export class TransactionHandler {
             resolve(res);
           }
         });
-      });
+    });
   };
 
-    listTransactionsByBondDid = (did: string) => {
-      return new Promise((resolve: Function, reject: Function) => {
-        return TransactionDB.aggregate([
-            {$match: {$or: [{"tx.body.messages.@type": "/bonds.MsgBuy"}, {"tx.body.messages.@type": "/bonds.MsgSell"}]}},
-            {$match: {"tx.body.messages.bond_did": did}},
-          ],
-          (err, res) => {
+  listTransactionsByBondDid = (did: string) => {
+    return new Promise((resolve: Function, reject: Function) => {
+      return TransactionDB.aggregate([
+        {$match: {
+            $or: [
+              {"tx.body.messages.@type": "/bonds.MsgBuy"}, {"tx.body.messages.@type": "/bonds.MsgSell"}
+            ]
+          }
+        },
+        {$match: {"tx.body.messages.bond_did": did}},
+      ],
+        (err, res) => {
           if (err) {
             reject(err);
-          } else {
+         } else {
             io.emit('list buys and sells for a given bond did', res);
             resolve(res);
-          }
+         }
         });
-      });
+    });
   };
 }
