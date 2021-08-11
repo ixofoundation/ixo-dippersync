aeb96a55aa1aab04519ad913031477dadbd2a2a9
stefaniatadama <stefania.damato.15@um.edu.mt>
2021-08-11 17:19:48 +0100

    Removed unnecessary blocksync files

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ require('log-timestamp');
 import * as http from 'http';
 import App from './app';
 import MongoUtils from './db/mongo_utils';
-import {SyncBlocks} from './util/sync_blocks';
 
 // Set the port
 const port = (process.env.PORT || 8080);
@@ -26,7 +25,3 @@ io.on('connection', function (socket) {
 let mongoDB = new MongoUtils(server, Number(port));
 
 mongoDB.connectToDb();
-
-let syncBlocks = new SyncBlocks();
-
-syncBlocks.startSync(chainUri, bcRest, bondsInfoExtractPeriod);
