b53f9d17ea86f1566d3eaa358700aa644ffc0485
stefaniatadama <stefania.damato.15@um.edu.mt>
2021-08-13 14:52:05 +0100

    More fixes

--- bin/start_no_build.sh
+++ bin/start_no_build.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# git clone https://github.com/ixofoundation/ixo-blocksync.git
-# cd ixo-blocksync
+# git clone https://github.com/ixofoundation/ixo-dippersync.git
+# cd ixo-dippersync
 
 # Note: this script should be run from outside the bin folder
 
 docker-compose up --no-start
-docker-compose start block-sync-db
 
-# attempting to wait for mongodb to be ready
-./bin/wait-for-service.sh block-sync-db 'waiting for connections on port' 10
-docker-compose start block-sync
+docker-compose start dipper-sync
 
-docker-compose logs --tail 13 block-sync
+docker-compose logs --tail 13 dipper-sync
 echo ""
 echo "***********************************"
-echo "* IXO BLOCKSYNC COMPLETE          *"
+echo "* IXO DIPPERSYNC COMPLETE          *"
 echo "***********************************"
 docker-compose ps
